98c057883f26bafc50b0565bf56bbe4651c41bff
Giovanni D'Introno <giovanni.dintrono@ext.esa.int>
2024-10-16 14:05:40 +0200

    adding comments

--- src/main/kotlin/com/esa/shortURL/ShortUrlService.kt
+++ src/main/kotlin/com/esa/shortURL/ShortUrlService.kt
@@ -11,6 +11,7 @@ import java.util.*
 class ShortUrlService(private val redis: StringRedisTemplate)
 {
 
+    // setting enconding
     private val digest = MessageDigest.getInstance("SHA-256")
 
     fun shortener(url: String): String {
